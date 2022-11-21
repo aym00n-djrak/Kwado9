46d0a70c30cd1b4ab569c304e02c9a3cb7e3c3dc
remy <remyj@outlook.fr>
2022-11-21 16:30:53 +0100

    feat: test button

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Button } from 'react-native';
+import { Alert } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Rémy App!</Text>
       <StatusBar style="auto" />
+      <Button title="Click me" onPress={() => Alert.alert('Button pressed!')} style={styles.button} />
     </View>
   );
 }
