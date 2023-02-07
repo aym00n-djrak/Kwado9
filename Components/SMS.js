a1ee63191586f2d7653672c36e493a624fb0411d
Aym00n <remyj@outlook.fr>
2023-02-07 14:48:09 +0100

    feat: sms

--- Components/SMS.js
+++ Components/SMS.js
@@ -1,9 +1,36 @@
-﻿import React, { useState, useRef } from 'react'
-import { StyleSheet, Text, View, TextInput, StatusBar, TouchableOpacity, Image, Alert, TouchableWithoutFeedback, Animated } from 'react-native'
+﻿import React, { useState, useRef } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  StatusBar,
+  TouchableOpacity,
+  Image,
+  Alert,
+  Button,
+  TouchableWithoutFeedback,
+  Animated,
+} from "react-native";
 import skull from "../assets/skull.png";
-import { TextAnimationFadeIn, TextAnimationZoom, TextAnimationRain, TextAnimationSlideDown, TextAnimationSlideUp, TextAnimationSlideLeft, TextAnimationSlideRight, TextAnimationShake, TextAnimationReverse, TextAnimationDeZoom } from 'react-native-text-effects';
+import {
+  TextAnimationFadeIn,
+  TextAnimationZoom,
+  TextAnimationRain,
+  TextAnimationSlideDown,
+  TextAnimationSlideUp,
+  TextAnimationSlideLeft,
+  TextAnimationSlideRight,
+  TextAnimationShake,
+  TextAnimationReverse,
+  TextAnimationDeZoom,
+} from "react-native-text-effects";
 
-import { LinearGradient } from 'expo-linear-gradient';
+import { LinearGradient } from "expo-linear-gradient";
+import { useEffect } from "react";
+import * as SMS from "expo-sms";
+import * as Print from "expo-print";
+import * as FileSystem from "expo-file-system";
 
 //inspiré de https://www.npmjs.com/package/react-native-text-effects
 // et de: https://www.rnexamples.com/react-native-examples/z/Contact-view
@@ -11,163 +38,218 @@ import { LinearGradient } from 'expo-linear-gradient';
 // et de: https://snack.expo.dev/@saad-bashar/scaledbutton
 
 export default function SMSScreen() {
+  const [isAvailable, setIsAvailable] = useState(false);
+  const [phoneNumber, setPhoneNumber] = useState(undefined);
+  const [recipients, setRecipients] = useState([]);
+  const [message, setMessage] = useState(undefined);
 
-    const [number, setNumber] = useState()
-    const [message, setMessage] = useState()
-    const [selected, setSelected] = useState(false)
-    const selectedAnim = useRef(new Animated.Value(1)).current;
-    return (
+  const selectedAnim = useRef(new Animated.Value(1)).current;
 
+  useEffect(() => {
+    async function checkAvailability() {
+      const isSmsAvailable = await SMS.isAvailableAsync();
+      setIsAvailable(isSmsAvailable);
+    }
+    checkAvailability();
+  }, []);
 
-        <View style={styles.container}>
+  const sendSms = async () => {
+    console.log("Generating pdf");
+    const { uri } = await Print.printToFileAsync({
+      html: "<h1>Hi friends</h1>",
+    });
 
+    console.log(uri);
 
-            <Image
-                style={styles.logo}
-                source={ skull }
-            />
+    const contentUri = await FileSystem.getContentUriAsync(uri);
+    console.log(contentUri);
 
-            <LinearGradient
-                // Background Linear Gradient
-                colors={['rgba(0,0,0,0.8)', 'transparent']}
-                style={styles.background}
-            />
-            <LinearGradient
-                // Button Linear Gradient
-                colors={['#003399', '#006666']}
-                style={styles.background}>
-                <TextAnimationFadeIn value={"Send an SMS!"} delay={100} duration={1000} style={{
-                    color: '#40e0d0', fontWeight: 'bold'
-                }} />
-            </LinearGradient>
+    const { result } = await SMS.sendSMSAsync(recipients, message, {
+      attachments: {
+        uri: contentUri,
+        mimeType: "application/pdf",
+        filename: "Hi.pdf",
+      },
+    });
 
-            <View style={styles.inputContainer}>
-                <Image
-                    style={styles.inputIcon}
-                    source={{ uri: 'https://img.icons8.com/color/70/000000/user.png' }}
-                />
+    console.log(result);
+  };
 
+  const addNumber = () => {
+    let newRecipients = [...recipients];
+    newRecipients.push(phoneNumber);
+    setRecipients(newRecipients);
+    setPhoneNumber(undefined);
+  };
 
-                <TextInput
-                    style={styles.textInput}
-                    placeholder="Number"
-                    keyboardType='numeric'
-                    underlineColorAndroid="transparent"
-                    maxLength={20}  //setting limit of input
-                    onChangeText={message => setNumber({ number })}
-                />
+  const showRecipients = () => {
+    if (recipients.length === 0) {
+      return <Text>No recipients added!</Text>;
+    }
 
-            </View>
+    return recipients.map((recipient, index) => {
+      return <Text key={index}>{recipient}</Text>;
+    });
+  };
 
-            <View style={styles.inputMessage}>
-                <Image
-                    style={styles.inputIcon}
-                    source={{ uri: 'https://img.icons8.com/color/70/000000/list.png' }}
-                />
-                <TextInput
-                    placeholder="Message"
-                    underlineColorAndroid="transparent"
-                    multiline={true}
-                    numberOfLines={4}
-                    onChangeText={message => setMessage({ message })}
-                />
-            </View>
-                <Animated.View style={[{ transform: [{ scale: selectedAnim }] }]}>
-                    <TouchableOpacity
-                        onPress={() => {
-                            Animated.sequence([
-                                Animated.timing(selectedAnim, {
-                                    toValue: 1.3,
-                                    duration: 300,
-                                    useNativeDriver: true,
-                                }),
-                                Animated.timing(selectedAnim, {
-                                    toValue: 1,
-                                    duration: 300,
-                                    useNativeDriver: true,
-                                })
-                            ]).start(() => setSelected(prev => !prev));
-                    }}
-                    style={styles.sendButton}
-                    >
-                        <Text style={styles.buttonText}>Send</Text>
-                    </TouchableOpacity>
-                </Animated.View>
-            <StatusBar style="light" />
+  return (
+    <View style={styles.container}>
+      <Image style={styles.logo} source={skull} />
+      <LinearGradient
+        // Background Linear Gradient
+        colors={["rgba(0,0,0,0.8)", "transparent"]}
+        style={styles.background}
+      />
+      <LinearGradient
+        // Button Linear Gradient
+        colors={["#003399", "#006666"]}
+        style={styles.background}
+      >
+        <TextAnimationFadeIn
+          value={"Send an SMS!"}
+          delay={100}
+          duration={1000}
+          style={{
+            color: "#40e0d0",
+            fontWeight: "bold",
+          }}
+        />
+      </LinearGradient>
+      <View style={styles.inputContainer}>
+        <Image
+          style={styles.inputIcon}
+          source={{ uri: "https://img.icons8.com/color/70/000000/user.png" }}
+        />
+        <TextInput
+          value={phoneNumber}
+          placeholder="Phone Number"
+          onChangeText={(value) => setPhoneNumber(value)}
+          style={styles.textInput}
+          />
+
+        <Button title="Add Number" onPress={addNumber} />
         </View>
-    );
+
+
+        <View style={styles.inputMessage}>
+
+     
+        <TextInput
+          value={message}
+          placeholder="Message"
+          underlineColorAndroid="transparent"
+          multiline={true}
+          numberOfLines={4}
+          onChangeText={(value) => setMessage(value)}
+          style={styles.inputMessage}
+        />
+
+</View>
+
+      {showRecipients()}
+
+      <Button title="Clear Recipients" onPress={() => setRecipients([])} />
+
+      {isAvailable ? (
+                        <Animated.View style={[{ transform: [{ scale: selectedAnim }] }]}>
+
+        <TouchableOpacity title="Send SMS"  onPress={() => {
+            Animated.sequence([
+                Animated.timing(selectedAnim, {
+                    toValue: 1.3,
+                    duration: 300,
+                    useNativeDriver: true,
+                }),
+                Animated.timing(selectedAnim, {
+                    toValue: 1,
+                    duration: 300,
+                    useNativeDriver: true,
+                })
+            ]).start(() => sendSms());
+    }}
+    style={styles.sendButton}>
+            <Text style={styles.sendButtonText}>Send SMS</Text>
+        </TouchableOpacity>
+        </Animated.View>
+      ) : (
+        <Text>SMS not available</Text>
+      )}
+
+      <StatusBar style="auto" />
+
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#003366',
-        width: '100%',
-        height: 200,
-    },
-    logo: {
-        width: 120,
-        height: 120,
-        justifyContent: 'center',
-        marginBottom: 20,
-    },
-    inputMessage: {
-        borderBottomColor: '#F5FCFF',
-        backgroundColor: '#f0ffff',
-        borderRadius: 10,
-        borderBottomWidth: 1,
-        width: 250,
-        height: 85,
-        marginBottom: 20,
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
-    inputContainer: {
-        borderBottomColor: '#F5FCFF',
-        backgroundColor: '#f0ffff',
-        borderRadius: 30,
-        borderBottomWidth: 1,
-        width: 250,
-        height: 45,
-        marginBottom: 20,
-        flexDirection: 'row',
-        alignItems: 'center',
-
-    },
-    inputs: {
-        height: 45,
-        marginLeft: 16,
-        borderBottomColor: '#FFFFFF',
-        flex: 1,
-    },
-    inputIcon: {
-        width: 30,
-        height: 30,
-        marginLeft: 15,
-        justifyContent: 'center',
-    },
-    buttonContainer: {
-        height: 45,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginBottom: 20,
-        width: 100,
-        borderRadius: 30,
-    },
-    sendButton: {
-        backgroundColor: '#26a7de',
-        height: 45,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginBottom: 20,
-        width: 100,
-        borderRadius: 30,
-    },
-    buttonText: {
-        color: 'white',
-    },
-})
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#003366",
+    width: "100%",
+    height: 200,
+  },
+  logo: {
+    width: 120,
+    height: 120,
+    justifyContent: "center",
+    marginBottom: 20,
+  },
+  inputMessage: {
+    borderBottomColor: "#F5FCFF",
+    backgroundColor: "#f0ffff",
+    borderRadius: 10,
+    borderBottomWidth: 1,
+    width: 250,
+    height: 85,
+    marginBottom: 20,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  inputContainer: {
+    borderBottomColor: "#F5FCFF",
+    backgroundColor: "#f0ffff",
+    borderRadius: 30,
+    borderBottomWidth: 1,
+    width: 250,
+    height: 45,
+    marginBottom: 20,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  inputs: {
+    height: 45,
+    marginLeft: 16,
+    borderBottomColor: "#FFFFFF",
+    flex: 1,
+  },
+  inputIcon: {
+    width: 30,
+    height: 30,
+    marginLeft: 15,
+    justifyContent: "center",
+  },
+  buttonContainer: {
+    height: 45,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 20,
+    width: 100,
+    borderRadius: 30,
+  },
+  sendButton: {
+    backgroundColor: "#26a7de",
+    height: 45,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 20,
+    width: 100,
+    borderRadius: 30,
+  },
+  buttonText: {
+    color: "white",
+  },
+});
