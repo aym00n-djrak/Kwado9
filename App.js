89d4586b8af2d61763ca57f7c9a9c5e1ffbee0c8
AlbertM96 <57848310+AlbertM96@users.noreply.github.com>
2023-02-07 15:47:24 +0100

    Accueil OK

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View,TextInput, Button, Alert } from 'react-native';
+import { StyleSheet, Text, Image, View,TextInput, Button, Alert } from 'react-native';
 import React from 'react';
 import Menu from './Components/Menu';
 import 'react-native-url-polyfill/auto';
@@ -9,6 +9,7 @@ import  {useState} from 'react';
 import { signOut } from 'firebase/auth';
 import { useEffect } from 'react';
 import { useRoute } from '@react-navigation/native';
+import { TextAnimationFadeIn, TextAnimationZoom, TextAnimationRain, TextAnimationSlideDown, TextAnimationSlideUp, TextAnimationSlideLeft, TextAnimationSlideRight, TextAnimationShake, TextAnimationReverse, TextAnimationDeZoom } from 'react-native-text-effects';
 
 export default function App() {
   const [email, setEmail] = useState('')
@@ -72,39 +73,42 @@ export default function App() {
   return (
     <>
     {login == 1 ? <Menu user =  {user} /> : 
-    <View style={styles.container}>
-        <Text>firebase : {login}</Text>
+              <View style={styles.container}>
+                  <Image
+                      source={{ uri: 'https://products.ls.graphics/mesh-gradients/images/02.-Egg-Sour.jpg' }}
+                      style={styles.background}
+                  />
+                  <View style={styles.formulaire}>
 
-        <Text>Email</Text>
+                      <TextAnimationShake value={"Kwado9"} delay={100} duration={1000} style={{
+                          color: '#f5f5f5', fontSize: 60, fontWeight: 'bold', opacity: 0.7
+                      }} />
+
+                      <Text style={styles.inputDescription}>Email</Text>
         <TextInput
             value={email}
             onChangeText={(email) => setEmail(email)}
             style={styles.input}
         />
 
-        <Text>Password</Text>
+                      <Text style={styles.inputDescription}>Password</Text>
         <TextInput
-            value={password}
+                          value={password}
+                          secureTextEntry={true}
             onChangeText={(password) => setPassword(password)}
             style={styles.input}
-        />
+                      />
 
-        <Button
-            title="Create Account"
-            onPress={handleCreateAccount}
-        />
+                  </View>
+                  <View style={styles.buttonBackground}>
+                      <Text style={styles.generateButton} onPress={handleCreateAccount}>Sign up</Text>
 
-        <Button
-            title="Sign In"
-            onPress={handleSignIn}
-        />
 
-        <Button
+                      <Text style={styles.generateButton} onPress={handleSignIn}>Sign in</Text>
+                      <Text style={styles.generateButton} onPress={handleSignOut}>Sign out</Text>
 
-            title="Sign Out"
-            onPress={handleSignOut}
-        />
-    </View>
+              </View>
+              </View>
 }
     </>
   );
@@ -112,17 +116,58 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+        flex: 1,
+        alignItems: "center",
+        backgroundColor: "#ffffff",
   },
-
+    formulaire: {
+        flex: 1,
+        //alignItems: "center",
+        marginTop: 170,
+        backgroundColor: 'transparent',
+        marginRight: 70,
+    },
   input: {
       borderWidth: 1,
-      borderColor: 'black',
-      padding: 10,
-      margin: 10,
-      width: 200
-  }
+      borderRadius: 8,
+      borderColor: '#191970',
+      padding: 8,
+      marginRight: 45,
+      margin: 7,
+      width: 230,
+    },
+    background: {
+        width: '100%',
+        height: '100%',
+        position: 'absolute',
+    },
+    inputDescription: {
+        color: "#191970",
+        fontSize: 20,
+        marginBottom: 0.5,
+        fontWeight: 'bold',
+        marginLeft: 8,
+    },
+    generateButton: {
+        //alignItems: 'center',
+        //justifyContent: 'center',
+        paddingVertical: 15,
+        paddingHorizontal: 29,
+        borderRadius: 20,
+        margin: 4,
+        elevation: 5,
+        marginRight: 220,
+        backgroundColor: '#483d8b',
+        color: "white",
+        fontWeight: 'bold',
+        opacity: 0.95,
+        fontSize: 15,
+
+    },
+    buttonBackground: {
+        marginBottom: 140,
+        marginLeft: 4,
+        background: "transparent",
+
+    },
 })
