74e03e59f4639cd68d8626fbc331cf23e3f577ae
Aym00n <remyj@outlook.fr>
2023-02-10 15:38:36 +0100

    fix: error

--- metro.config.js
+++ metro.config.js
@@ -2,6 +2,10 @@
 const { getDefaultConfig } = require('@expo/metro-config');
 
 const defaultConfig = getDefaultConfig(__dirname);
+defaultConfig.resolver.extraNodeModules = {
+  ...defaultConfig.resolver.extraNodeModules,
+  ...require("expo-crypto-polyfills")
+};
 defaultConfig.resolver.assetExts.push('cjs');
 
 module.exports = defaultConfig;
